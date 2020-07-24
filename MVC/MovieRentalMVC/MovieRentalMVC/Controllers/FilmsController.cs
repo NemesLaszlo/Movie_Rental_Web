1c8df7840025807b1264d2ea7af93a2dfbe5df02
NemesLaszlo <wow.laszlo@gmail.com>
2020-07-24 19:24:26 +0200

    Edit Update possibility

--- MVC/MovieRentalMVC/MovieRentalMVC/Controllers/FilmsController.cs
+++ MVC/MovieRentalMVC/MovieRentalMVC/Controllers/FilmsController.cs
@@ -20,9 +20,53 @@ namespace MovieRentalMVC.Controllers
         {
             _db = db;
         }
+
+        // load all films to the table because of GetAll and the js script
         public IActionResult Index()
         {
             return View();
+        }
+
+        // navigate to the create or update page
+        public IActionResult Upsert(int? id)
+        {
+            Film = new Film();
+            if(id == null)
+            {
+                // Create
+                return View(Film);
+            }
+
+            // Update
+            Film = _db.Films.FirstOrDefault(u => u.Id == id);
+            if (Film == null)
+            {
+                return NotFound();
+            }
+
+            return View(Film);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Film.Id == 0)
+                {
+                    // Create
+                    _db.Films.Add(Film);
+                }
+                else
+                {
+                    // Update
+                    _db.Films.Update(Film);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Film);
         }
 
         [HttpGet]
@@ -35,7 +79,6 @@ namespace MovieRentalMVC.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             var filmFromDb = await _db.Films.FirstOrDefaultAsync(u => u.Id == id);
-
             if (filmFromDb == null)
             {
                 return Json(new { success = false, message = "Error while Deleting" });
