0cefed24a2c25245e46c22ae512f80b8d02f22d9
NemesLaszlo <wow.laszlo@gmail.com>
2020-07-24 18:45:20 +0200

    FilmsController Get and Delete + Frontend table

--- MVC/MovieRentalMVC/MovieRentalMVC/Controllers/FilmsController.cs
+++ MVC/MovieRentalMVC/MovieRentalMVC/Controllers/FilmsController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieRentalMVC.Models;
 
 namespace MovieRentalMVC.Controllers
 {
     public class FilmsController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Film Film { get; set; }
+
+        public FilmsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Films.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var filmFromDb = await _db.Films.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (filmFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+
+            _db.Films.Remove(filmFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
         }
     }
 }
---
+++ MVC/MovieRentalMVC/MovieRentalMVC/wwwroot/js/filmList.js
@@ -0,0 +1,65 @@
+﻿let dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/Films/getall/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "title", "width": "20%" },
+            { "data": "director", "width": "20%" },
+            { "data": "year", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Films/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Edit
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/Films/Delete?id='+${data})>
+                            Delete
+                        </a>
+                        </div>`;
+                }, "width": "40%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
